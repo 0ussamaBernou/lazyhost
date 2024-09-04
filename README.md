3a9770dcd5e5f55e2429f4054014696ecfefc196
0ussamabernou <oussama.bernou@outlook.com>
2024-09-05 00:56:03 +0100

    v0.0.2

---
+++ README.md
@@ -0,0 +1,25 @@
+# LazyHost
+
+A simple CLI tool to create a local reverse proxy server for your localhost website or app to test on any device before deploying to production.
+
+## Installation
+
+```bash
+npm install lazyhost -g
+```
+
+## Usage
+
+```bash
+lazyhost
+```
+
+## Or run it directly
+
+```bash
+npx lazyhost
+```
+
+## License
+
+MIT
--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,41 @@
+#!/usr/bin/env node
+
 import inquirer from "inquirer";
 import { program } from "commander";
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import localtunnel from "localtunnel";
-import ngrok from "ngrok";
 import ip from "ip";
 import chalk from "chalk";
 import qrcode from "qrcode";
+import boxen from "boxen";
+import process from "process";
+import { warn } from "console";
+
+const useragent =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
+
+process.on("SIGINT", () => {
+  console.log(chalk.red("\nProcess interrupted. Exiting..."));
+  process.exit();
+});
+
+process.on("SIGTERM", () => {
+  console.log(chalk.red("\nProcess terminated. Exiting..."));
+  process.exit();
+});
 
 const generateQRCode = async (text, label) => {
   try {
-    const url = await qrcode.toString(text, { type: "terminal" });
+    const url = await qrcode.toString(text, { type: "terminal", small: true });
+    // url = url
+    //   .split("\n")
+    //   .map((line) =>
+    //     line.padStart((process.stdout.columns - line.length) / 2 + line.length),
+    //   )
+    //   .join("\n");
     console.log(chalk.green(`${label} QR Code:`));
-    console.log(url);
+    console.log(boxen(url, { padding: 1, borderColor: "green" }));
   } catch (err) {
     console.error(chalk.red("Failed to generate QR Code:"), err);
   }
@@ -30,7 +53,7 @@ const startReverseProxy = (port, targetPort) => {
   app.listen(port, "0.0.0.0", async () => {
     const localAddress = `http://${ip.address()}:${port}`;
     console.log(chalk.green(`Reverse proxy started on ${localAddress}`));
-    console.log(chalk.yellow(`Forwarding to http://localhost:${targetPort}`));
+    console.log(chalk.yellow(`Forwarding http://localhost:${targetPort}`));
     await generateQRCode(localAddress, "Local Address");
   });
 };
@@ -50,20 +73,26 @@ const startLocaltunnel = async (port) => {
   const tunnel = await localtunnel({ port });
   console.log(chalk.blue(`localtunnel Tunnel started on ${tunnel.url}`));
   await generateQRCode(tunnel.url, "localtunnel Tunnel");
+  fetch("https://loca.lt/mytunnelpassword", {
+    headers: { UserAgent: useragent },
+  })
+    .then((res) => res.text())
+    .then((password) => {
+      console.log(chalk.yellow(`Tunnel password: ${password}`));
+    });
   tunnel.on("close", () => {
     console.log(chalk.red("localtunnel Tunnel closed"));
   });
 };
 
 program
-  .version("1.0.0")
+  .version("0.0.0")
   .description("A CLI tool for reverse proxy and localtunnel/ngrok Tunnels")
   .option("-p, --port <number>", "Port to run the reverse proxy on")
   .option("-t, --target <number>", "Target port of the localhost server")
   .option("-l, --localtunnel", "Start a Localtunnel tunnel")
-  .option("-n, --ngrok", "Start an ngrok tunnel")
   .action(async (options) => {
-    let port, targetPort;
+    let useLocaltunnel, port, targetPort;
     if (!options.port || !options.target) {
       const answers = await inquirer.prompt([
         {
@@ -82,20 +111,24 @@ program
           validate: (input) =>
             !isNaN(parseInt(input)) || "Please enter a valid number",
         },
+        {
+          type: "confirm",
+          name: "localtunnel",
+          message:
+            "Do you want to use localtunnel(accessible outside your network)?",
+          default: false,
+        },
       ]);
       port = parseInt(answers.port);
       targetPort = parseInt(answers.targetPort);
+      useLocaltunnel = answers.localtunnel;
     } else {
       port = parseInt(options.port);
       targetPort = parseInt(options.target);
     }
     startReverseProxy(port, targetPort);
 
-    if (options.ngrok) {
-      await startNgrokTunnel(port);
-    }
-
-    if (options.localtunnel) {
+    if (options.localtunnel || useLocaltunnel) {
       await startLocaltunnel(port);
     }
   });
